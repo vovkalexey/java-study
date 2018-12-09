8112bfa2a2e766d483bcf1f93c11416ff52a6ce2
Alexey Vovk <vovkalexey@gmail.com>
2018-12-09 20:35:40 +0200

    Memory exercise

---
+++ memory-management/src/main/java/ua/dp/vovkalexey/memory/mangement/exercise/Container.java
@@ -0,0 +1,13 @@
+package ua.dp.vovkalexey.memory.mangement.exercise;
+
+public class Container {
+	private String initial = "A";
+	
+	public String getInitial() {
+		return initial;
+	}
+	
+	public void setInitial(String initial) {
+		this.initial = initial;
+	}
+}
