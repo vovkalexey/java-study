6fab8589d2e460b6cbb50a4a05a3aab04adf1bf1
Alexey Vovk <vovkalexey@gmail.com>
2019-01-05 17:19:55 +0200

    Ch.11 Escaping References Exercise - clear;

--- memory-management/src/main/java/ua/dp/vovkalexey/memory/mangement/escaping/exercise/BookCollection.java
+++ memory-management/src/main/java/ua/dp/vovkalexey/memory/mangement/escaping/exercise/BookCollection.java
@@ -1,12 +1,7 @@
 package ua.dp.vovkalexey.memory.mangement.escaping.exercise;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class BookCollection {
 	
