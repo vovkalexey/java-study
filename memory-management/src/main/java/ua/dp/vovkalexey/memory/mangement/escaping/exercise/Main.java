cd8410f810856c179bc90ad461bc0a9acfe96192
Alexey Vovk <vovkalexey@gmail.com>
2019-01-05 17:01:40 +0200

    Ch.11 Escaping References Exercise - fix convert() method;

--- memory-management/src/main/java/ua/dp/vovkalexey/memory/mangement/escaping/exercise/Main.java
+++ memory-management/src/main/java/ua/dp/vovkalexey/memory/mangement/escaping/exercise/Main.java
@@ -10,9 +10,12 @@ public class Main {
 	public static void main(String[] args) {
 
 		BookCollection bc = new BookCollection();
+
+		bc.printAllBooks();
 		
 		//get price of book called Tom Jones in EUR
 		System.out.println(bc.findBookByName("Tom Jones").getPrice().convert("EUR"));
-		
+
+		bc.printAllBooks();
 	}
 }
--- memory-management/src/main/java/ua/dp/vovkalexey/memory/mangement/escaping/exercise/Price.java
+++ memory-management/src/main/java/ua/dp/vovkalexey/memory/mangement/escaping/exercise/Price.java
@@ -16,7 +16,6 @@ public class Price {
 		rates.put("EUR", 0.8);
 	}
 		
-	//TODO: We appear to be mutating a value in a get method
 	public Double convert(String toCurrency) {
 		
 		if (toCurrency.equals("USD")) {
@@ -24,8 +23,7 @@ public class Price {
 		}
 		else {
 			Double conversion = rates.get("USD") / rates.get(toCurrency);
-			value = conversion * value;
-			return value;
+			return conversion * value;
 		}
 	}
 	
