7889d14731477faa23a99a2ccd2c05ee705f5260
Alexey Vovk <vovkalexey@gmail.com>
2019-11-16 21:38:02 +0200

    add method for create developer by spec-param

--- java-Go4/src/main/java/ua/dp/vovkalexey/java/Go4/DesignPatterns/Creational/FactoryMethod/Program.java
+++ java-Go4/src/main/java/ua/dp/vovkalexey/java/Go4/DesignPatterns/Creational/FactoryMethod/Program.java
@@ -3,12 +3,23 @@ package ua.dp.vovkalexey.java.Go4.DesignPatterns.Creational.FactoryMethod;
 public class Program {
 
     public static void main(String[] args) {
-        DeveloperFactory jFactory = new JavaDeveloperFactory();
+        DeveloperFactory jFactory = createDeveloperBySpec("java");
         Developer developer1 = jFactory.createDeveloper();
         developer1.writeCode();
 
-        DeveloperFactory cFactory = new CppDeveloperFactory();
+        DeveloperFactory cFactory = createDeveloperBySpec("c++");
         Developer developer2 = cFactory.createDeveloper();
         developer2.writeCode();
+    }
+
+    static DeveloperFactory createDeveloperBySpec(String specialty) {
+        if (specialty.equalsIgnoreCase("java")) {
+            return new JavaDeveloperFactory();
+        } else if (specialty.equalsIgnoreCase("c++")) {
+            return new CppDeveloperFactory();
+        } else {
+            throw new RuntimeException(specialty + " is unknown specialty");
+        }
+
     }
 }
